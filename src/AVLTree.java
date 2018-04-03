60cbb12351320a63c4ce12e4ae091cff043497fe
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-04-02 19:07:23 -0700

    null pointer bug because of y node not having a child

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -227,6 +227,12 @@ public class AVLTree<E extends Comparable<E>>
 		   int a = (tmp.left == null) ? -1 : tmp.left.height;
 		   int b = (tmp.right == null) ? -1 : tmp.right.height;
 		   tmp.height = 1 + Math.max(a, b);
+		   int c = Math.abs(a) - Math.abs(b);
+		   if(c > 1) {
+			   System.out.println(c + " " + tmp.item);
+			   restructure(tmp);
+			   
+		   }
 		   tmp = tmp.parent;
 	   }
    }
@@ -238,7 +244,7 @@ public class AVLTree<E extends Comparable<E>>
 	   else {
 		   int a = (t.right == null) ? -1 : t.right.height;
 		   int b = (t.left == null) ? -1 : t.left.height;
-		   int c = Math.abs((a-b));
+		   int c = Math.abs(a) - Math.abs(b);
 		   if( c > 1) 
 			   return false;
 		   
@@ -246,7 +252,46 @@ public class AVLTree<E extends Comparable<E>>
 			   return (isHeightBalanced(t.left) && isHeightBalanced(t.right));
 	   }
    }
-   
+    
+   private AVLNode<E> restructure(AVLNode<E> t) {
+	   
+	   AVLNode<E> x = t;
+	   
+	   AVLNode<E> y = (x.left.height > x.right.height) ? x.left : x.right;
+	   System.out.println();
+	   AVLNode<E> z;
+	   if(y.left.height > y.right.height)
+		   z = y.left;
+	   else if(y.left.height == y.right.height) {
+		   if(y.item == x.left.item)
+			   z = y.left;
+		   else
+			   z = y.right;
+		   
+	   }
+	   else
+		   z = y.right;
+	   AVLNode<E> top = null;
+	   
+	  int c = whichCase(x, y, z);
+	  if(c == 0) {
+		  top = case_0(x,y,z);
+		  
+	  }
+	  if(c == 1) {
+		 top = case_1(x,y,z);
+		  
+	  }
+	  if(c == 2) {
+		 top = case_2(x,y,z);
+	  }
+	  if(c == 3) {
+		 top = case_3(x,y,z);
+	  }
+	  
+	  return top;
+	  
+   }
              
      /***************** private remove helper methods ***************************************/
    
@@ -323,8 +368,127 @@ public class AVLTree<E extends Comparable<E>>
       }
       return p;
    }
-     
    
+  private int whichCase(AVLNode<E> x, AVLNode<E> y, AVLNode<E> z) {
+	  if((x.left.item.compareTo(y.item) == 0) && (y.left.item.compareTo(z.item) == 0))
+		  return 0;
+	  if((x.right.item.compareTo(y.item) == 0) && (y.right.item.compareTo(z.item) == 0))
+		  return 1;
+	  if((x.left.item.compareTo(y.item) == 0) && (y.right.item.compareTo(z.item) == 0))
+		  return 2;
+	  if((x.right.item.compareTo(y.item) == 0) && (y.left.item.compareTo(z.item) == 0))
+		  return 3;
+	  else
+		  return -1;
+  }
+  private AVLNode<E> case_0(AVLNode<E> x, AVLNode<E> y, AVLNode<E> z) {
+	  AVLNode<E> t1 = z.left;
+	  AVLNode<E> t2 = z.right;
+	  AVLNode<E> t3 = y.right;
+	  AVLNode<E> t4 = x.right;
+	  AVLNode<E> parent = (x.parent == null) ? null : x.parent;
+	  if(parent == null) 
+		  root = y;
+	  else {
+		  if(parent.right.item == x.item) {
+			  parent.right = y;
+		  }
+		  else {
+			  parent.left = y;
+		  }
+	  }
+	  y.left = z;
+	  z.left = t1;
+	  z.right = t2;
+	  y.right = x;
+	  x.left = t3;
+	  x.right = t4;
+	  x.parent = y;
+	  z.parent = y;
+	  return x.parent;
+		  
+	  
+  }
+  
+  private AVLNode<E> case_1(AVLNode<E> x, AVLNode<E> y, AVLNode<E> z) {
+	  AVLNode<E> t1 = x.left;
+	  AVLNode<E> t2 = y.left;
+	  AVLNode<E> t3 = z.left;
+	  AVLNode<E> t4 = z.right;
+	  AVLNode<E> parent = (x.parent == null) ? null : x.parent;
+	  if(parent == null) 
+		  root = y;
+	  else {
+		  if(parent.right.item == x.item) {
+			  parent.right = y;
+		  }
+		  else {
+			  parent.left = y;
+		  }
+	  }
+	  y.left = x;
+	  x.left = t1;
+	  x.right = t2;
+	  y.right = z;
+	  z.left = t3;
+	  z.right = t4;
+	  x.parent = y;
+	  z.parent = y;
+	  return x.parent;
+	  
+  }
+  private AVLNode<E> case_2(AVLNode<E> x, AVLNode<E> y, AVLNode<E> z) {
+	  AVLNode<E> t1 = y.left;
+	  AVLNode<E> t2 = z.left;
+	  AVLNode<E> t3 = z.right;
+	  AVLNode<E> t4 = x.right;
+	  AVLNode<E> parent = (x.parent == null) ? null : x.parent;
+	  if(parent == null) 
+		  root = z;
+	  else {
+		  if(parent.right.item == x.item) {
+			  parent.right = z;
+		  }
+		  else {
+			  parent.left = z;
+		  }
+	  }
+	  z.left = y;
+	  y.left = t1;
+	  y.right = t2;
+	  z.right = x;
+	  x.left = t3;
+	  x.right = t4;
+	  x.parent = z;
+	  y.parent = z;
+	  return x.parent;
+  }
+  private AVLNode<E> case_3(AVLNode<E> x, AVLNode<E> y, AVLNode<E> z) {
+	  AVLNode<E> t1 = x.left;
+	  AVLNode<E> t2 = z.left;
+	  AVLNode<E> t3 = z.right;
+	  AVLNode<E> t4 = y.right;
+	  AVLNode<E> parent = (x.parent == null) ? null : x.parent;
+	  if(parent == null) 
+		  root = z;
+	  else {
+		  if(parent.right.item == x.item) {
+			  parent.right = z;
+		  }
+		  else {
+			  parent.left = z;
+		  }
+	  }
+	  z.left = x;
+	  x.left = t1;
+	  x.right = t2;
+	  z.right = y;
+	  y.left = t3;
+	  y.right = t4;
+	  x.parent = z;
+	  y.parent = z;
+	  return x.parent;
+  }
                
    //private traversal methods      
            
